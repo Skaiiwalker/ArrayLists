f5e4e05b6481a9ef90ccf39ea606c93975f31dc8
Skaiiwalker <lbar8299@740s-pl-130.local>
2015-10-07 09:56:28 -0600

    yo son

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,9 +1,31 @@
 package lists.controller;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
+		firstWords.add("zero");
+		firstWords.add("one");
+		showList();
 		
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null,  firstWords.get(spot) + " is at spot# " + spot);
+		}
 	}
 }
